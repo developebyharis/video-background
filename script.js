faefc22474dadac42d01f3fe33f7131276757740
Muhammad Haris Khan <developewithharis@gmail.com>
2022-07-21 12:49:58 +0500

    Second commit

--- script.js
+++ script.js
@@ -1,24 +1,12 @@
-// var Video = document.getElementById("myVideo");
-// var btn = document.getElementById("myBtn");
-// function myFunction() {
-//     if (Video.paused) {
-//         Video.play();
-//         btn.innerHTML = "Pause";
-//     } else {
-//         Video.pause();
-//         btn.innerHTML = "PLay";
-//     }
-// }
-
 var Video = document.getElementById("myVideo");
 var btn = document.getElementById("myBtn");
 function myFunction() {
     if (Video.paused) {
-    Video.play();
-    btn.innerHTML = "Pause";
-
-} else {
-    Video.pause();
-    btn.innerHTML = "Play";
-  }
+        Video.play();
+        btn.innerHTML = "Pause";
+    } else {
+        Video.pause();
+        btn.innerHTML = "PLay";
+    }
 }
+
